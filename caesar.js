161d0dd1e50b1132bd2c0961a8935b1e6f0fc8a5
Darwon Rashid <40280334@live.napier.ac.uk>
2018-02-19 22:50:45 +0000

    19/02/2018 - Hill Cipher initialized. Updated Style. polished caesar cipher. Helper functions left for Hill

--- caesar.js
+++ caesar.js
@@ -1,48 +1,97 @@
+/*TODO FUCING FIX*/
 
+function  encrypt()
+{
+    var text = document.getElementById('encryptInput').value;
+    var key = document.getElementById('encryptKey').value;
 
-function  Caesar(plainText, key) {
-  var text = plainText;
+    key = parseInt(key);
 
+    var cipher = '';
 
-  if(key < 0)
-  {
-    Caesar(plainText, key + 26);
-  }
-  else
-  {
+    if(!Number.isInteger(key))
+    {
+      alert("key is not an integer");
+  		return;
+    }
 
-    cipherText = '';
+    if (key < 0 || key >= 26) {
+    	alert("Shift is out of range");
+    	return;
+    }
 
     for(var i = 0; i < text.length; i++)
     {
-      var c = text[i];
-
-      if (c.match(/[a-z]/i))
-      {
         var code = text.charCodeAt(i);
 
         if((code >= 65) && (code <= 90))
         {
-          c = String.fromCharCode(((code - 65 + key) % 26) + 65);
+          cipher += String.fromCharCode(((code - 65 + key) % 26) + 65);
         }
         else if((code >= 97) && (code <= 122))
         {
-            c = String.fromCharCode(((code - 97 + key) % 26) + 97);
+            cipher += String.fromCharCode(((code - 97 + key) % 26) + 97);
+        }
+        else
+        {
+          cipher += text.charCodeAt(i);
         }
 
+    }
+      document.getElementById('encryptKey').value = '';
+      document.getElementById('decryptKey').value = key;
+      document.getElementById('decryptInput').value = cipher;
+      document.getElementById('encryptInput').value = '';
+}
+
+function decrypt()
+{
+
+      var text = document.getElementById('decryptInput').value;
+      var key = document.getElementById('decryptKey').value;
+
+      key = parseInt(key);
+
+      var cipher = '';
+
+      if(!Number.isInteger(key))
+      {
+        alert("key is not an integer");
+    		return;
+      }
+
+      if (key < 0 || key >= 26) {
+      	alert("Shift is out of range");
+      	return;
+      }
+
+      for(var i = 0; i < text.length; i++)
+      {
+          var code = text.charCodeAt(i);
+
+          if((code >= 65) && (code <= 90))
+          {
+            cipher += String.fromCharCode(((code - 90 - key) % 26) + 90);
+          }
+          else if((code >= 97) && (code <= 122))
+          {
+              cipher += String.fromCharCode(((code - 122 - key) % 26) + 122);
+          }
+          else
+          {
+            cipher += text.charCodeAt(i);
+          }
 
       }
 
-      cipherText += c;
-    }
+      document.getElementById('encryptKey').value = key;
+      document.getElementById('decryptKey').value = '';
+      document.getElementById('encryptInput').value = cipher;
+      document.getElementById('decryptInput').value = '';
+
+}
 
 
-    document.getElementById('decryptInput').value = cipherText;
-
-  }
-
-
-};
 
 
 /* Set the width of the side navigation to 250px */
---
+++ hill.js
@@ -0,0 +1,27 @@
+
+/* Set the width of the side navigation to 250px */
+function openNav()
+{
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav()
+{
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+/*HILL CIPHER ALGORITHM DOWN HERE*/
+
+function encrypt()
+{
+
+}
+
+function decrypt()
+{
+  
+}
+
+/*HELPER FUNCTIONS FOR HILL CPHER*/
