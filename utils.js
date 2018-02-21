5eebba0e4d7198dff02d04d394573e4d3bbf4bba
Darwon Rashid <40280334@live.napier.ac.uk>
2018-02-21 19:01:47 +0000

    Responsive Design done

--- utils.js
+++ utils.js
@@ -2,12 +2,14 @@
 function openNav()
 {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("navContainer").style.width = "0px";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav()
 {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("navContainer").style.width = "0px";
 }
 
 
